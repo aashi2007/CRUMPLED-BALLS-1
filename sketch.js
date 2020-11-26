1d3f681a5b3669af4dae4fc58d009b60f2470ee2
aashi2007 <71537319+aashi2007@users.noreply.github.com>
2020-11-26 18:00:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustbinObj, paperObject,groundObject;	
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	dustbinObj=new Dustbin(1200,650);
+	paperObject=new Paper(200,450,40);
+	groundObject=new Ground(width/2,670,width,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+   background(0);
+  
+   dustbinObj.display();
+   paperObject.display();
+   groundObject.display();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+  
+	}
+}
+
